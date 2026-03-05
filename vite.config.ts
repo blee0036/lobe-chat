470d5e62552b32ce9eca0f3fda0e147610f32c20
ChrisWilson <jzm@outlook.sk>
2026-03-05 18:10:05 +0800

    fix build memory

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   base: isDev ? '/' : process.env.VITE_CDN_BASE || '/spa/',
   build: {
     outDir: isMobile ? 'dist/mobile' : 'dist/desktop',
+    reportCompressedSize: false,
     rollupOptions: {
       input: resolve(__dirname, isMobile ? 'index.mobile.html' : 'index.html'),
       output: sharedRollupOutput,
